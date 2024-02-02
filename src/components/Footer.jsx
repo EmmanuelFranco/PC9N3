ea4ac007283fb74ff2f9374a4be156b52b664cf2
Emmanuel Franco <efranco56@misena.edu.co>
2024-02-01 23:15:30 -0500

    Practica Calificada 9 Nivel 3

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+export function Footer () {
+
+
+  return (
+    <footer className='footer'>
+      <h4>Practica de Página e-commerce en React ⚛️</h4>
+      <h4><span>@efranco107 (Emmanuel Franco)</span></h4> 
+      <h5>E-coomerce sencilla, para agregar articulos al carrito.</h5>
+    </footer>
+  )
+}
